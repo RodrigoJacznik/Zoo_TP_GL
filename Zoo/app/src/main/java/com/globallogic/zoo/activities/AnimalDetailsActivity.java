c57c1924822fc4c2c49bb1a3490875d0a35ee16b
Jacznik Rodrigo <jacznik.rodrigo@gmail.com>
2015-03-31 17:57:54 -0300

    Agrega boton de share y camara a un animal.

--- Zoo/app/src/main/java/com/globallogic/zoo/activities/AnimalDetailsActivity.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/activities/AnimalDetailsActivity.java
@@ -2,7 +2,11 @@ package com.globallogic.zoo.activities;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
+import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -23,12 +27,18 @@ import com.globallogic.zoo.custom.views.FavoriteView;
 import com.globallogic.zoo.models.Animal;
 import com.globallogic.zoo.models.Schudle;
 
+import org.apache.http.protocol.HTTP;
+
+import java.io.File;
+
 
 public class AnimalDetailsActivity extends ActionBarActivity implements FavoriteViewCallback {
     public final static String ANIMAL = "ANIMAL";
 
     private final static String FAVORITE = "FAVORITE";
     private final static String COLOR = "COLOR";
+
+    private static final int REQUEST_CAMERA = 0;
 
     private TextView name;
     private TextView specie;
@@ -57,50 +67,27 @@ public class AnimalDetailsActivity extends ActionBarActivity implements Favorite
         btnMoreInfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (checkConnection()) {
-                    Intent intent = new Intent(AnimalDetailsActivity.this, MoreInfoActivity.class);
-                    intent.putExtra(MoreInfoActivity.URL, animal.getUrl());
-                    startActivity(intent);
-                } else {
-                    Toast.makeText(AnimalDetailsActivity.this, "Problemas en su conexion", Toast.LENGTH_LONG).show();
-                }
-           }
+                moreInfo();
+            }
         });
 
         photo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AnimalDetailsActivity.this, "photo", Toast.LENGTH_SHORT).show();
+                takePhoto();
             }
         });
 
         share.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(AnimalDetailsActivity.this, "share", Toast.LENGTH_SHORT).show();
+                shareAnimal();
             }
         });
 
         favoriteView.setFavoriteState(animal.isFavorite());
         favoriteView.setCallback(this);
         populateScheduleTable();
-    }
-
-    private void bindViews() {
-        animal = (Animal) getIntent().getSerializableExtra(WelcomeActivity.ANIMAL);
-        favoriteView = (FavoriteView) findViewById(R.id.animaldetailsactivity_fav);
-        name = (TextView) findViewById(R.id.animaldetailsactivity_name);
-        specie = (TextView) findViewById(R.id.animaldetailsactivity_specie);
-        description = (TextView) findViewById(R.id.animaldetailsactivity_description);
-        schedule = (TableLayout) findViewById(R.id.animaldetailsactivity_table);
-        rootView = findViewById(R.id.animaldetailsactivity_scrollview);
-        share = (ImageView) findViewById(R.id.animaldetailsactivity_share);
-        photo = (ImageView) findViewById(R.id.animaldetailsactivity_photo);
-    }
-
-    private boolean checkConnection() {
-        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
-        return (cm.getActiveNetworkInfo() != null);
     }
 
     @Override
@@ -117,6 +104,77 @@ public class AnimalDetailsActivity extends ActionBarActivity implements Favorite
         super.onSaveInstanceState(outState);
         outState.putBoolean(FAVORITE, animal.isFavorite());
         outState.putInt(COLOR, favoriteViewColor);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch (requestCode) {
+            case REQUEST_CAMERA:
+                if (resultCode == RESULT_OK) {
+                    File f = new File(Environment.getExternalStorageDirectory(), "temp.jpg");
+                    if (f.exists()) {
+                        sendMail(f);
+                    }
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void moreInfo() {
+        if (checkConnection()) {
+            Intent intent = new Intent(this, MoreInfoActivity.class);
+            intent.putExtra(MoreInfoActivity.URL, animal.getUrl());
+            startActivity(intent);
+        } else {
+            Toast.makeText(
+                    this,
+                    getResources().getString(R.string.animaldetailsactivity_without_connection),
+                    Toast.LENGTH_LONG).show();
+        }
+    }
+
+    private void shareAnimal() {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.putExtra(Intent.EXTRA_TEXT, animal.getName() + ": " + animal.getDescripcion());
+        intent.setType(HTTP.PLAIN_TEXT_TYPE);
+        startActivity(intent);
+    }
+
+    private void takePhoto() {
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        File f = new File(android.os.Environment.getExternalStorageDirectory(), "temp.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(f));
+        startActivityForResult(intent, REQUEST_CAMERA);
+    }
+
+    private void sendMail(File file) {
+        Intent emailIntent = new Intent(Intent.ACTION_SEND);
+        emailIntent.setType("application/image");
+        String subject = String.format(getResources().
+                getString(R.string.animaldetailsactivity_email_subject), animal.getName());
+        emailIntent.putExtra(Intent.EXTRA_SUBJECT, subject);
+        emailIntent.putExtra(Intent.EXTRA_STREAM, Uri.fromFile(file));
+        startActivity(emailIntent);
+    }
+
+    private void bindViews() {
+        animal = (Animal) getIntent().getSerializableExtra(WelcomeActivity.ANIMAL);
+        favoriteView = (FavoriteView) findViewById(R.id.animaldetailsactivity_fav);
+        name = (TextView) findViewById(R.id.animaldetailsactivity_name);
+        specie = (TextView) findViewById(R.id.animaldetailsactivity_specie);
+        description = (TextView) findViewById(R.id.animaldetailsactivity_description);
+        schedule = (TableLayout) findViewById(R.id.animaldetailsactivity_table);
+        rootView = findViewById(R.id.animaldetailsactivity_scrollview);
+        share = (ImageView) findViewById(R.id.animaldetailsactivity_share);
+        photo = (ImageView) findViewById(R.id.animaldetailsactivity_photo);
+    }
+
+    private boolean checkConnection() {
+        ConnectivityManager cm = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        return (cm.getActiveNetworkInfo() != null);
     }
 
     private void populateScheduleTable() {
