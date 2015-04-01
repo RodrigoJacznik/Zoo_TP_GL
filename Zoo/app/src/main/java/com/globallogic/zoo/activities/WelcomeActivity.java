6087ab7513c46eb0e89a48a4ae08968fafa3295b
Jacznik Rodrigo <jacznik.rodrigo@gmail.com>
2015-04-01 10:07:11 -0300

    Agrega boton para ver posicion en maps.

--- Zoo/app/src/main/java/com/globallogic/zoo/activities/WelcomeActivity.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/activities/WelcomeActivity.java
@@ -1,13 +1,16 @@
 package com.globallogic.zoo.activities;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.globallogic.zoo.R;
 import com.globallogic.zoo.adapters.AnimalAdapter;
@@ -21,6 +24,7 @@ public class WelcomeActivity extends ActionBarActivity implements AnimalAdapterC
 
     private Button signout;
     private TextView welcome;
+    private ImageView maps;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,6 +45,28 @@ public class WelcomeActivity extends ActionBarActivity implements AnimalAdapterC
             }
         });
 
+        maps.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                viewPositionInMaps();
+            }
+        });
+
+        bindRecyclerView();
+    }
+
+    private void viewPositionInMaps() {
+        Intent mapsIntent = new Intent(Intent.ACTION_VIEW);
+        mapsIntent.setData(Uri.parse("geo:-34.9085327, -57.9378387"));
+        if (mapsIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(mapsIntent);
+        } else {
+            Toast.makeText(this, getResources().getString(R.string.welcomeactivity_without_maps),
+                    Toast.LENGTH_SHORT).show();
+        }
+    }
+
+    private void bindRecyclerView() {
         RecyclerView mRecyclerView = (RecyclerView) findViewById(R.id.welcomeactivity_recycleview);
         RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setLayoutManager(mLayoutManager);
@@ -56,6 +82,7 @@ public class WelcomeActivity extends ActionBarActivity implements AnimalAdapterC
     private void bindViews() {
         signout = (Button) findViewById(R.id.welcomeactivity_signout);
         welcome = (TextView) findViewById(R.id.welcomeactivity_welcome);
+        maps = (ImageView) findViewById(R.id.welcomeactivity_maps);
     }
 
     @Override
