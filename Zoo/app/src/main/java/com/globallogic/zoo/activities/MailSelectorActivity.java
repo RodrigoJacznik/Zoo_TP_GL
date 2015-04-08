2ba53322245533ead9a742025374abf462f2790a
Jacznik Rodrigo <jacznik.rodrigo@gmail.com>
2015-04-08 15:01:00 -0300

    Agrega intent para devolver el mail al share dialog.

--- Zoo/app/src/main/java/com/globallogic/zoo/activities/MailSelectorActivity.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/activities/MailSelectorActivity.java
@@ -1,14 +1,18 @@
 package com.globallogic.zoo.activities;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
 
 import com.globallogic.zoo.R;
+import com.globallogic.zoo.custom.views.ShareDialog;
 
 import java.util.ArrayList;
 
@@ -23,9 +27,17 @@ public class MailSelectorActivity extends ActionBarActivity {
         ListView listView = (ListView) findViewById(R.id.mailselectoractivity_emails);
         ArrayList<String> emails = getIntent().getStringArrayListExtra(EMAILS);
 
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+        final ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1, emails);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent intent = new Intent();
+                intent.putExtra(ShareDialog.EMAIL, adapter.getItem(position));
+                setResult(ShareDialog.RESULT_OK, intent);
+                finish();
+            }
+        });
         listView.setAdapter(adapter);
-
     }
 }
