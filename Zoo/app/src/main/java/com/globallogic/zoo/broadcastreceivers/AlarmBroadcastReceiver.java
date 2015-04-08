508ada9836d710571b1125564844c20a64ebd7a4
Jacznik Rodrigo <jacznik.rodrigo@gmail.com>
2015-04-08 15:05:49 -0300

    Refactorea onReceive.

--- Zoo/app/src/main/java/com/globallogic/zoo/broadcastreceivers/AlarmBroadcastReceiver.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/broadcastreceivers/AlarmBroadcastReceiver.java
@@ -8,7 +8,6 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Vibrator;
 import android.support.v4.app.NotificationCompat;
-import android.util.Log;
 
 import com.globallogic.zoo.R;
 import com.globallogic.zoo.activities.AnimalDetailsActivity;
@@ -26,18 +25,45 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
-        vibrator.vibrate(1000);
-
+        vibrate(context, 1000);
         int animalId = intent.getIntExtra(ANIMAL_ID, -1);
+        makeNotification(context, animalId);
+    }
+
+    private void makeNotification(Context context, int animalId) {
+        int notificationId = 1;
+        PendingIntent pendingIntent = getPendingIntent(context, animalId);
+
         String content = String.format(context.getString(R.string.notification_content),
                 Animal.getAnimalList().get(animalId).getName());
+
+        NotificationCompat.Builder builder = getAnimalNotificationBuilder(context,
+                pendingIntent, content);
+
+        NotificationManager notificationManager =
+                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(notificationId, builder.build());
+    }
+
+    private void vibrate(Context context, int time) {
+        Vibrator vibrator = (Vibrator) context.getSystemService(Context.VIBRATOR_SERVICE);
+        vibrator.vibrate(time);
+    }
+
+    private NotificationCompat.Builder getAnimalNotificationBuilder(
+            Context context, PendingIntent pendingIntent, String content) {
 
         NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
                 .setSmallIcon(R.drawable.sheep)
                 .setContentTitle(context.getString(R.string.notification_title))
                 .setContentText(content);
 
+        builder.setContentIntent(pendingIntent);
+        builder.setAutoCancel(true);
+        return builder;
+    }
+
+    private PendingIntent getPendingIntent(Context context, int animalId) {
         Intent resultIntent = new Intent(context, AnimalDetailsActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
 
@@ -45,14 +71,7 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         stackBuilder.addNextIntent(resultIntent);
         resultIntent.putExtra(AnimalDetailsActivity.ANIMAL, animalId);
 
-        PendingIntent pendingIntent = stackBuilder.getPendingIntent(0,
+        return stackBuilder.getPendingIntent(0,
                 PendingIntent.FLAG_UPDATE_CURRENT);
-
-        builder.setContentIntent(pendingIntent);
-
-        int notificationId = 001;
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(notificationId, builder.build());
     }
 }
