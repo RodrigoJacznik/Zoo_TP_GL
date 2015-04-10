656253f5b4aa5fad93c026817d004407b6218540
Jacznik Rodrigo <jacznik.rodrigo@gmail.com>
2015-04-10 11:43:01 -0300

    Agrega notificacion

--- Zoo/app/src/main/java/com/globallogic/zoo/broadcastreceivers/AlarmBroadcastReceiver.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/broadcastreceivers/AlarmBroadcastReceiver.java
@@ -11,6 +11,7 @@ import android.support.v4.app.NotificationCompat;
 
 import com.globallogic.zoo.R;
 import com.globallogic.zoo.activities.AnimalDetailsActivity;
+import com.globallogic.zoo.activities.WelcomeActivity;
 import com.globallogic.zoo.models.Animal;
 
 /**
@@ -18,6 +19,9 @@ import com.globallogic.zoo.models.Animal;
  */
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
     public static final String ANIMAL_ID = "ANIMAL_ID";
+    public static final String MULTI_NOTIFICATION = "MULTI_NOTIFICATION";
+
+    private static int notificationCount = 0;
 
     public AlarmBroadcastReceiver() {
         super();
@@ -26,20 +30,34 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
         vibrate(context, 1000);
-        int animalId = intent.getIntExtra(ANIMAL_ID, -1);
+
+        long animalId = intent.getLongExtra(ANIMAL_ID, -1);
         makeNotification(context, animalId);
     }
 
-    private void makeNotification(Context context, int animalId) {
+    private void makeNotification(Context context, long animalId) {
         int notificationId = 1;
-        PendingIntent pendingIntent = getPendingIntent(context, animalId);
 
         String content = String.format(context.getString(R.string.notification_content),
-                Animal.getAnimalList().get(animalId).getName());
+                Animal.getById(animalId).getName());
 
-        NotificationCompat.Builder builder = getAnimalNotificationBuilder(context,
-                pendingIntent, content);
+        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
+                .setSmallIcon(R.drawable.sheep)
+                .setContentTitle(context.getString(R.string.notification_title))
+                .setContentText(content);
 
+        builder.setAutoCancel(true)
+               .setNumber(notificationCount++);
+
+        PendingIntent pendingIntent;
+
+        if (notificationCount > 1) {
+            pendingIntent = getWelcomeActivityPendingIntent(context);
+        } else {
+            pendingIntent = getAnimalDetailPendingIntent(context, animalId);
+        }
+
+        builder.setContentIntent(pendingIntent);
         NotificationManager notificationManager =
                 (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
         notificationManager.notify(notificationId, builder.build());
@@ -50,20 +68,7 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         vibrator.vibrate(time);
     }
 
-    private NotificationCompat.Builder getAnimalNotificationBuilder(
-            Context context, PendingIntent pendingIntent, String content) {
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.sheep)
-                .setContentTitle(context.getString(R.string.notification_title))
-                .setContentText(content);
-
-        builder.setContentIntent(pendingIntent);
-        builder.setAutoCancel(true);
-        return builder;
-    }
-
-    private PendingIntent getPendingIntent(Context context, int animalId) {
+    private PendingIntent getAnimalDetailPendingIntent(Context context, long animalId) {
         Intent resultIntent = new Intent(context, AnimalDetailsActivity.class);
         TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
 
@@ -73,5 +78,17 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
 
         return stackBuilder.getPendingIntent(0,
                 PendingIntent.FLAG_UPDATE_CURRENT);
+    }
+
+    private PendingIntent getWelcomeActivityPendingIntent(Context context) {
+        Intent resultIntent = new Intent(context, WelcomeActivity.class);
+        resultIntent.putExtra(MULTI_NOTIFICATION, true);
+        return PendingIntent.getActivity(context, 0, resultIntent, 0);
+    }
+
+    static public void resetNotificationCount(Boolean bool) {
+        if (bool) {
+            notificationCount = 0;
+        }
     }
 }
