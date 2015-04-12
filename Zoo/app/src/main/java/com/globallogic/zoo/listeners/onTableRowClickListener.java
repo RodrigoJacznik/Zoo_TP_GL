40d92354a0fa87019f63dd5c5a44dedec75f8505
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2015-04-11 22:35:42 -0300

    Cambio con respecto a 78a2738

--- Zoo/app/src/main/java/com/globallogic/zoo/listeners/onTableRowClickListener.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/listeners/onTableRowClickListener.java
@@ -1,18 +1,12 @@
 package com.globallogic.zoo.listeners;
 
-import android.content.ContentResolver;
 import android.content.Context;
-import android.database.Cursor;
-import android.net.Uri;
-import android.provider.CalendarContract;
 import android.view.View;
 import android.widget.Toast;
 
 import com.globallogic.zoo.R;
 import com.globallogic.zoo.models.Schedule;
-import com.globallogic.zoo.models.Show;
-
-import java.util.concurrent.ScheduledExecutorService;
+import com.globallogic.zoo.helpers.ContentProviderHelper;
 
 /**
  * Created by GL on 07/04/2015.
@@ -21,18 +15,6 @@ public class onTableRowClickListener implements View.OnClickListener {
     private Schedule schedule;
     private Context context;
 
-    public static final String[] EVENT_PROYECTION = new String[] {
-            CalendarContract.Events._ID,
-            CalendarContract.Events.TITLE,
-            CalendarContract.Events.DTSTART,
-            CalendarContract.Events.DTEND,
-    };
-
-    private static final int EVENT_ID_INDEX = 0;
-    private static final int EVENT_TITLE_INDEX = 1;
-    private static final int EVENT_DTSTART_INDEX = 2;
-    private static final int EVENT_DTEND_INDEX = 3;
-
     public onTableRowClickListener(Schedule schedule, Context context) {
         this.schedule = schedule;
         this.context = context;
@@ -40,36 +22,15 @@ public class onTableRowClickListener implements View.OnClickListener {
 
     @Override
     public void onClick(View v) {
-        Cursor cur;
-        ContentResolver cr = context.getContentResolver();
-        Uri uri = CalendarContract.Events.CONTENT_URI;
+        String eventName = ContentProviderHelper.checkUserHasAnEvent(context, schedule);
 
-        String selection = "(" +
-                "( ? between " + CalendarContract.Events.DTSTART +
-                " AND " + CalendarContract.Events.DTEND + ")" +
-                    " OR " +
-                "( ? between " + CalendarContract.Events.DTSTART +
-                " AND " + CalendarContract.Events.DTEND + ")" +
-                    " OR " +
-                "( " + CalendarContract.Events.DTSTART + " between ? AND ? ))";
-
-        String start = String.valueOf(schedule.getInitialHour().getTime());
-        String end = String.valueOf(schedule.getFinalHour().getTime());
-
-        String[] selectionargs = new String[] {start, end, start, end};
-
-        cur = cr.query(uri, EVENT_PROYECTION, selection, selectionargs, null);
-
-        if (cur.getCount() < 1) {
+        if (eventName == null) {
             Toast.makeText(context,
                     context.getString(R.string.animaldetailsactivity_without_event),
                     Toast.LENGTH_SHORT).show();
         } else {
-            cur.moveToNext();
-            String event_title = cur.getString(EVENT_TITLE_INDEX);
             String template = context.getString(R.string.animaldetailsactivity_with_event);
-            Toast.makeText(context, String.format(template, event_title), Toast.LENGTH_LONG).show();
+            Toast.makeText(context, String.format(template, eventName), Toast.LENGTH_LONG).show();
         }
-        cur.close();
     }
 }
