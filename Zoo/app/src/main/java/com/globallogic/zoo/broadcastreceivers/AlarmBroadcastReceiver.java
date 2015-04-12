afa380554ffe72a30c7d058a042ea572f69f4844
RodrigoJacznik <jacznik.rodrigo@gmail.com>
2015-04-11 22:36:59 -0300

    Se extrea metodo para enviar notificaciones

--- Zoo/app/src/main/java/com/globallogic/zoo/broadcastreceivers/AlarmBroadcastReceiver.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/broadcastreceivers/AlarmBroadcastReceiver.java
@@ -1,27 +1,22 @@
 package com.globallogic.zoo.broadcastreceivers;
 
-import android.app.NotificationManager;
+import android.app.AlarmManager;
 import android.app.PendingIntent;
-import android.app.TaskStackBuilder;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
+import android.os.SystemClock;
 import android.os.Vibrator;
-import android.support.v4.app.NotificationCompat;
+import android.util.Log;
 
-import com.globallogic.zoo.R;
-import com.globallogic.zoo.activities.AnimalDetailsActivity;
-import com.globallogic.zoo.activities.WelcomeActivity;
-import com.globallogic.zoo.models.Animal;
+import com.globallogic.zoo.helpers.NotificationHelper;
 
 /**
  * Created by GL on 06/04/2015.
  */
 public class AlarmBroadcastReceiver extends BroadcastReceiver {
     public static final String ANIMAL_ID = "ANIMAL_ID";
-    public static final String MULTI_NOTIFICATION = "MULTI_NOTIFICATION";
-
-    private static int notificationCount = 0;
+    private static int alarmCount = 0;
 
     public AlarmBroadcastReceiver() {
         super();
@@ -32,35 +27,8 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         vibrate(context, 1000);
 
         long animalId = intent.getLongExtra(ANIMAL_ID, -1);
-        makeNotification(context, animalId);
-    }
-
-    private void makeNotification(Context context, long animalId) {
-        int notificationId = 1;
-
-        String content = String.format(context.getString(R.string.notification_content),
-                Animal.getById(animalId).getName());
-
-        NotificationCompat.Builder builder = new NotificationCompat.Builder(context)
-                .setSmallIcon(R.drawable.sheep)
-                .setContentTitle(context.getString(R.string.notification_title))
-                .setContentText(content);
-
-        builder.setAutoCancel(true)
-               .setNumber(notificationCount++);
-
-        PendingIntent pendingIntent;
-
-        if (notificationCount > 1) {
-            pendingIntent = getWelcomeActivityPendingIntent(context);
-        } else {
-            pendingIntent = getAnimalDetailPendingIntent(context, animalId);
-        }
-
-        builder.setContentIntent(pendingIntent);
-        NotificationManager notificationManager =
-                (NotificationManager) context.getSystemService(Context.NOTIFICATION_SERVICE);
-        notificationManager.notify(notificationId, builder.build());
+        Log.d("AlarmBroadcastReceiver", "Recibi " + String.valueOf(animalId));
+        NotificationHelper.makeNotification(context, animalId);
     }
 
     private void vibrate(Context context, int time) {
@@ -68,26 +36,15 @@ public class AlarmBroadcastReceiver extends BroadcastReceiver {
         vibrator.vibrate(time);
     }
 
-    private PendingIntent getAnimalDetailPendingIntent(Context context, long animalId) {
-        Intent resultIntent = new Intent(context, AnimalDetailsActivity.class);
-        TaskStackBuilder stackBuilder = TaskStackBuilder.create(context);
+    public static void sendVibrateBroadcast(Context context, int seconds, long animalId) {
+        AlarmManager alarmManager = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(context, AlarmBroadcastReceiver.class);
 
-        stackBuilder.addParentStack(AnimalDetailsActivity.class);
-        stackBuilder.addNextIntent(resultIntent);
-        resultIntent.putExtra(AnimalDetailsActivity.ANIMAL, animalId);
+        intent.putExtra(ANIMAL_ID, animalId);
 
-        return stackBuilder.getPendingIntent(0, PendingIntent.FLAG_UPDATE_CURRENT);
-    }
+        PendingIntent alarmIntent = PendingIntent.getBroadcast(context, alarmCount++, intent, 0);
 
-    private PendingIntent getWelcomeActivityPendingIntent(Context context) {
-        Intent resultIntent = new Intent(context, WelcomeActivity.class);
-        resultIntent.putExtra(MULTI_NOTIFICATION, true);
-        return PendingIntent.getActivity(context, 0, resultIntent, 0);
-    }
-
-    static public void resetNotificationCount(Boolean bool) {
-        if (bool) {
-            notificationCount = 0;
-        }
+        alarmManager.set(AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                SystemClock.elapsedRealtime() + seconds * 1000, alarmIntent);
     }
 }
