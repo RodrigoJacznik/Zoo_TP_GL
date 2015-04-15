88e13a07eb47a5b9aa8dd1909d18bbd9711359bc
Jacznik Rodrigo <jacznik.rodrigo@gmail.com>
2015-04-15 16:06:21 -0300

    Agrega nuevos metodos para acceder a la DB.

--- Zoo/app/src/main/java/com/globallogic/zoo/helpers/ZooDatabaseHelper.java
+++ Zoo/app/src/main/java/com/globallogic/zoo/helpers/ZooDatabaseHelper.java
@@ -5,9 +5,9 @@ import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
 import com.globallogic.zoo.models.Animal;
-import com.globallogic.zoo.models.dao.AnimalDB;
-import com.globallogic.zoo.models.dao.AnimalShowDB;
-import com.globallogic.zoo.models.dao.ShowDB;
+import com.globallogic.zoo.models.dao.AnimalDAO;
+import com.globallogic.zoo.models.dao.AnimalShowDAO;
+import com.globallogic.zoo.models.dao.ShowDAO;
 
 import java.util.List;
 
@@ -27,28 +27,30 @@ public class ZooDatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL(AnimalDB.CREATE_TABLE);
-        db.execSQL(ShowDB.CREATE_TABLE);
-        db.execSQL(AnimalShowDB.CREATE_TABLE);
+        db.execSQL(AnimalDAO.CREATE_TABLE);
+        db.execSQL(ShowDAO.CREATE_TABLE);
+        db.execSQL(AnimalShowDAO.CREATE_TABLE);
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL(AnimalDB.DROP_TABLE);
-        db.execSQL(ShowDB.DROP_TABLE);
-        db.execSQL(AnimalShowDB.DROP_TABLE);
+        db.execSQL(AnimalDAO.DROP_TABLE);
+        db.execSQL(ShowDAO.DROP_TABLE);
+        db.execSQL(AnimalShowDAO.DROP_TABLE);
 
-        db.execSQL(AnimalDB.CREATE_TABLE);
-        db.execSQL(ShowDB.CREATE_TABLE);
-        db.execSQL(AnimalShowDB.CREATE_TABLE);
+        db.execSQL(AnimalDAO.CREATE_TABLE);
+        db.execSQL(ShowDAO.CREATE_TABLE);
+        db.execSQL(AnimalShowDAO.CREATE_TABLE);
     }
 
     public void insertAnimal(Animal animal) {
-        AnimalDB.getOrInsert(this, animal);
+        AnimalDAO.getOrInsert(this, animal);
     }
 
+    public void insertOrUpdateAnimal(Animal animal) { AnimalDAO.insertOrUpdate(this, animal); }
+
     public Animal getAnimalById(long animalId) {
-        return AnimalDB.get(this, animalId);
+        return AnimalDAO.get(this, animalId);
     }
 
     public void insertAnimals(final List<Animal> animals) {
@@ -63,5 +65,9 @@ public class ZooDatabaseHelper extends SQLiteOpenHelper {
         });
 
         thread.start();
+    }
+
+    public List<Animal> getAnimals() {
+        return AnimalDAO.getAll(this);
     }
 }
